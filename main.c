f8c0d0d4b5a065042f33e75f27f84735fa2494dd
trindadedev13 <trindadedev13@gmail.com>
2026-01-13 20:46:21 -0300

    cube
    Signed-off-by: trindadedev13 <trindadedev13@gmail.com>

--- main.c
+++ main.c
@@ -2,29 +2,34 @@
 #include <stdio.h>
 
 int
-main ()
+main (void)
 {
-  Camera3D camera;
-  Vector3 cube_pos;
+  const int win_width = 800;
+  const int win_height= 800;
+  Camera3D camera = {0};
+  Vector3 cube_pos = { 0.f, 0.f, 0.f };
 
-  camera.position = (Vector3) {0.f, 10.f, 10.f};
-  camera.target = (Vector3) {0.f, 0.f, 0.f};
-  camera.up = (Vector3) {0.f, 1.f, 0.f};
+  camera.position = (Vector3){ 0.f, 10.f, 10.f };
+  camera.target = (Vector3){ 0.f, 0.f, 0.f };
+  camera.up = (Vector3){ 0.f, 1.f, 0.f };
   camera.fovy = 45.f;
   camera.projection = CAMERA_PERSPECTIVE;
 
-  
-  InitWindow (720, 360, "teste");
+  InitWindow (win_width, win_height, "teste");
+  DisableCursor ();
   SetTargetFPS (60);
   while (!WindowShouldClose ())
   {
+    UpdateCamera (&camera, CAMERA_FREE);
     BeginDrawing ();
 
     ClearBackground (WHITE);
 
-    BeginMode3D(camera);
+    BeginMode3D (camera);
+      DrawCube (cube_pos, 2.f, 2.f, 2.f, BLUE);
+      DrawCubeWires(cube_pos, 2.f, 2.f, 2.f, MAROON);
       DrawGrid (10, 1.f);
-    EndMode3D();
+    EndMode3D ();
 
     EndDrawing ();
   }
