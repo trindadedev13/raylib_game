06d1274e320fd918d3a1a0e2b3f787b73962104c
Thiarley Rocha <rthiarley@gmail.com>
2026-01-13 23:19:03 -0300

    add sun

--- main.c
+++ main.c
@@ -24,13 +24,49 @@ IDrawGrid (int slices, float spacing, Color color)
   rlEnd ();
 }
 
+void DrawSun(int *dir, Vector3 *pos){
+
+  float SPEED = 0.04f;
+
+  switch(*dir){
+    case 0:
+      if(pos->x > 10.0f){
+        *dir = 1;
+        break;
+      }
+      pos->x += SPEED;
+    break;
+    case 1:
+      if(pos->y > 10.0f){
+        *dir = 2;
+        break;
+      }
+      pos->y += SPEED;
+    break;
+    case 2:
+      if(pos->x < 0.0f){
+        *dir = 3;
+        break;
+      }
+      pos->x -= SPEED;
+    break;
+    case 3:
+      if(pos->y < 0.0f){
+        *dir = 0;
+        break;
+      }
+      pos->y -= SPEED;
+    break;
+  }
+}
+
 int
 main (void)
 {
   const int win_width = 800;
   const int win_height = 800;
   Camera3D camera = { 0 };
-  Vector3 cube_pos = { 0.f, 0.f, 0.f };
+  Vector3 cube_pos = { 0.f, 1.f, 0.f };
 
   camera.position = (Vector3){ 0.f, 10.f, 10.f };
   camera.target = (Vector3){ 0.f, 0.f, 0.f };
@@ -43,19 +79,27 @@ main (void)
   SetTargetFPS (60);
 
   Vector3 terrain_pos = { 0.f, 0.f, 0.f };
-  Vector2 terrain_size = { 10.f, 1.0f };
+  Vector2 terrain_size = { 10.f, 10.0f };
+
+  int direction = 0;
+  Vector3 sun_pos = {0,10.0f,10.0f};
 
   while (!WindowShouldClose ())
   {
     UpdateCamera (&camera, CAMERA_FREE);
     BeginDrawing ();
 
-    ClearBackground (WHITE);
+    ClearBackground ((Color){0,0,20});
 
     BeginMode3D (camera);
     DrawCube (cube_pos, 2.f, 2.f, 2.f, BLUE);
     DrawCubeWires (cube_pos, 2.f, 2.f, 2.f, MAROON);
+
+    DrawCircle3D((Vector3){0.0f,10.0f,0.0f}, 1.0f, (Vector3){0.0f,0.0f,0.0f},0.0f, YELLOW);
+    DrawSun(&direction, &sun_pos);
+    DrawSphere(sun_pos, 1.0f, YELLOW);
     DrawPlane (terrain_pos, terrain_size, GREEN);
+
     IDrawGrid (10, 1.f, BLACK);
     EndMode3D ();
 
