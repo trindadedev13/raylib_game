5e52290b02b50abacbdf6c7f61cd32b91a3d09d1
trindadedev13 <trindadedev13@gmail.com>
2026-01-14 00:48:53 -0300

    feat (Android): Use full Display size at android
    Signed-off-by: trindadedev13 <trindadedev13@gmail.com>

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <raylib.h>
 #include <rlgl.h>
-#include <stdio.h>
 
 void
 IDrawGrid (int slices, float spacing, Color color)
@@ -24,40 +23,47 @@ IDrawGrid (int slices, float spacing, Color color)
   rlEnd ();
 }
 
-void DrawSun(int *dir, Vector3 *pos){
+void
+DrawSun (int *dir, Vector3 *pos)
+{
 
   float SPEED = 0.05f;
   float MIN = 0.0f;
   float MAX = 10.0f;
 
-  switch(*dir){
-    case 0:
-      if(pos->x > MAX){
-        *dir = 1;
-        break;
-      }
-      pos->x += SPEED;
+  switch (*dir)
+  {
+  case 0:
+    if (pos->x > MAX)
+    {
+      *dir = 1;
+      break;
+    }
+    pos->x += SPEED;
     break;
-    case 1:
-      if(pos->y > MAX){
-        *dir = 2;
-        break;
-      }
-      pos->y += SPEED;
+  case 1:
+    if (pos->y > MAX)
+    {
+      *dir = 2;
+      break;
+    }
+    pos->y += SPEED;
     break;
-    case 2:
-      if(pos->x < MIN){
-        *dir = 3;
-        break;
-      }
-      pos->x -= SPEED;
+  case 2:
+    if (pos->x < MIN)
+    {
+      *dir = 3;
+      break;
+    }
+    pos->x -= SPEED;
     break;
-    case 3:
-      if(pos->y < MIN){
-        *dir = 0;
-        break;
-      }
-      pos->y -= SPEED;
+  case 3:
+    if (pos->y < MIN)
+    {
+      *dir = 0;
+      break;
+    }
+    pos->y -= SPEED;
     break;
   }
 }
@@ -65,41 +71,46 @@ void DrawSun(int *dir, Vector3 *pos){
 int
 main (void)
 {
-  const int win_width = 800;
-  const int win_height = 800;
   Camera3D camera = { 0 };
-  Vector3 cube_pos = { 0.f, 1.f, 0.f };
-
   camera.position = (Vector3){ 0.f, 10.f, 10.f };
   camera.target = (Vector3){ 0.f, 0.f, 0.f };
   camera.up = (Vector3){ 0.f, 1.f, 0.f };
   camera.fovy = 45.f;
   camera.projection = CAMERA_PERSPECTIVE;
 
+  // aqui vou definir o tamanho da janela de acordo com o OS
+  // se for ANDROID vai ser fullscreen, caso contrario 800x800
+  int win_width = 800, win_height = 800;
+#ifdef __ANDROID__
+  win_width = GetScreenWidth ();
+  win_height = GetScreenHeight ();
+#endif
   InitWindow (win_width, win_height, "teste");
   DisableCursor ();
   SetTargetFPS (60);
 
   Vector3 terrain_pos = { 0.f, 0.f, 0.f };
   Vector2 terrain_size = { 10.f, 10.0f };
+  Vector3 cube_pos = { 0.f, 1.f, 0.f };
 
   int direction = 0;
-  Vector3 sun_pos = {0,10.0f,10.0f};
+  Vector3 sun_pos = { 0, 10.0f, 10.0f };
 
   while (!WindowShouldClose ())
   {
     UpdateCamera (&camera, CAMERA_FREE);
     BeginDrawing ();
 
-    ClearBackground ((Color){0,0,20});
+    ClearBackground ((Color){ 0, 0, 20, 255});
 
     BeginMode3D (camera);
     DrawCube (cube_pos, 2.f, 2.f, 2.f, BLUE);
     DrawCubeWires (cube_pos, 2.f, 2.f, 2.f, MAROON);
 
-    DrawCircle3D((Vector3){0.0f,10.0f,0.0f}, 1.0f, (Vector3){0.0f,0.0f,0.0f},0.0f, YELLOW);
-    DrawSun(&direction, &sun_pos);
-    DrawSphere(sun_pos, 1.0f, YELLOW);
+    DrawCircle3D ((Vector3){ 0.0f, 10.0f, 0.0f }, 1.0f,
+                  (Vector3){ 0.0f, 0.0f, 0.0f }, 0.0f, YELLOW);
+    DrawSun (&direction, &sun_pos);
+    DrawSphere (sun_pos, 1.0f, YELLOW);
     DrawPlane (terrain_pos, terrain_size, GREEN);
 
     IDrawGrid (10, 1.f, BLACK);
