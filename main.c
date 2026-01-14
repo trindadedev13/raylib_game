5c878586a66d8575805a9006c7c3593a8e79537c
trindadedev13 <trindadedev13@gmail.com>
2026-01-13 21:49:44 -0300

    feat: add colored grid
    Signed-off-by: trindadedev13 <trindadedev13@gmail.com>

--- main.c
+++ main.c
@@ -1,12 +1,35 @@
 #include <raylib.h>
+#include <rlgl.h>
 #include <stdio.h>
+
+void
+IDrawGrid (int slices, float spacing, Color color)
+{
+  int halfSlices = slices / 2;
+
+  rlBegin (RL_LINES);
+  for (int i = -halfSlices; i <= halfSlices; i++)
+  {
+    rlColor3f (color.r, color.g, color.b);
+    rlColor3f (color.r, color.g, color.b);
+    rlColor3f (color.r, color.g, color.b);
+    rlColor3f (color.r, color.g, color.b);
+
+    rlVertex3f ((float)i * spacing, 0.0f, (float)-halfSlices * spacing);
+    rlVertex3f ((float)i * spacing, 0.0f, (float)halfSlices * spacing);
+
+    rlVertex3f ((float)-halfSlices * spacing, 0.0f, (float)i * spacing);
+    rlVertex3f ((float)halfSlices * spacing, 0.0f, (float)i * spacing);
+  }
+  rlEnd ();
+}
 
 int
 main (void)
 {
   const int win_width = 800;
-  const int win_height= 800;
-  Camera3D camera = {0};
+  const int win_height = 800;
+  Camera3D camera = { 0 };
   Vector3 cube_pos = { 0.f, 0.f, 0.f };
 
   camera.position = (Vector3){ 0.f, 10.f, 10.f };
@@ -19,12 +42,8 @@ main (void)
   DisableCursor ();
   SetTargetFPS (60);
 
-  Vector3 terrain_pos = {
-    0.f, 0.f, 0.f
-  };
-  Vector2 terrain_size = {
-    10.f, 1.0f
-  };
+  Vector3 terrain_pos = { 0.f, 0.f, 0.f };
+  Vector2 terrain_size = { 10.f, 1.0f };
 
   while (!WindowShouldClose ())
   {
@@ -34,10 +53,10 @@ main (void)
     ClearBackground (WHITE);
 
     BeginMode3D (camera);
-      DrawCube (cube_pos, 2.f, 2.f, 2.f, BLUE);
-      DrawCubeWires(cube_pos, 2.f, 2.f, 2.f, MAROON);
-      DrawPlane(terrain_pos, terrain_size, GREEN);
-      DrawGrid (10, 1.f);
+    DrawCube (cube_pos, 2.f, 2.f, 2.f, BLUE);
+    DrawCubeWires (cube_pos, 2.f, 2.f, 2.f, MAROON);
+    DrawPlane (terrain_pos, terrain_size, GREEN);
+    IDrawGrid (10, 1.f, BLACK);
     EndMode3D ();
 
     EndDrawing ();
