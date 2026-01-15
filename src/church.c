e558c7f6ae04477f62796d805a093bb725fb22df
trindadedev13 <trindadedev13@gmail.com>
2026-01-15 15:11:07 -0300

    feat: Aumentar tamanho da igreja
    Signed-off-by: trindadedev13 <trindadedev13@gmail.com>

--- src/church.c
+++ src/church.c
@@ -14,7 +14,7 @@ LoadChurch (Church *church) {
 
 void
 DrawChurch (Church *church) {
-  DrawModel (church->model, church->pos, 0.1f, WHITE);
+  DrawModel (church->model, church->pos, 0.5f, WHITE);
 }
 
 void
